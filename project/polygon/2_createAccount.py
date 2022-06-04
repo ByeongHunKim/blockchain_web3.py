c4399fbbba140dc60eed121d01214237ab198f0e
ByeongHun <you@example.com>
2022-06-04 23:57:41 +0900

    #3 Polygon 1~3 upload

---
+++ project/polygon/2_createAccount.py
@@ -0,0 +1,44 @@
+from web3 import Web3, IPCProvider, HTTPProvider
+from base58 import b58encode, b58decode
+
+web3 = Web3(HTTPProvider("https://matic-mumbai.chainstacklabs.com"))
+print("1. 폴리곤 연결여부:", isConnected)
+
+# privKey = "c4bc573075f7dad8dcfc9b9f5c800e94572b0da1497ade9c7d5836df8aeccde2"
+
+# account = web3.eth.accounts.privateKeyToAccount(privKey)
+
+# acct = web3.eth.account.create(privKey)
+
+# publicKey = acct.address
+# secretKey = acct.privateKey
+
+# print('publickey: ', publicKey)
+# print('privkey: ', secretKey)
+
+#print('privkey: ', acct.privateKey)
+
+#makePrivKey = secretKey + secretKey
+
+#realPrivKey = b64encode(makePrivKey).decode()
+
+#print('realPrivKey: ', realPrivKey)
+
+# print("2. privkey: ", privKey)
+# print("3. account: ", account)
+
+# acct = web3.eth.account.create("asd123!")
+# print('addr: ', acct.address)
+
+
+# 두번째로 생성한 지갑주소 0xf1337471403D2D7E7a83166c5E48dd619ecc95c3
+# 이 계정이 asd123! 으로 tx 전송이 가능한지.. 그리고 privKey는 어떻게 아는지? 
+# 메타마스크 연동을 위함 
+
+# balanceOfAddr = web3.eth.getBalance("0xab3a34C0039560184493e5eB852aad4213E62e8D")
+
+# print(balanceOfAddr)
+
+# eth2Matic = web3.fromWei(balanceOfAddr, "ether")
+
+# print(eth2Matic)
---
+++ project/polygon/3_sendMatic.py
@@ -0,0 +1,47 @@
+from web3 import Web3, IPCProvider, HTTPProvider
+
+# ()
+# web3 = Web3(IPCProvider("/home/bstudent/geth_project/rinkeby/geth.ipc"))
+
+## https://matic-mumbai.chainstacklabs.com
+web3 = Web3(HTTPProvider("https://matic-mumbai.chainstacklabs.com"))
+print("1. 폴리곤 연결여부:", isConnected)
+
+Addr = "0xC1F72d2436f6f23384c2d035e509f795450C2434"
+privKey = "priv_key"
+AddrChecksum = web3.toChecksumAddress(Addr)
+print("2. checkSum - 확인여부 :", AddrChecksum)
+print("2.1 privKey - 확인여부 :", privKey)
+
+getGasPrice = web3.eth.gasPrice
+print("3. 가스비 - 확인여부 :", getGasPrice)
+
+balanceOfAddr = web3.eth.getBalance(Addr)
+print("4. 잔액 - 확인여부 :", balanceOfAddr)
+
+nonce = web3.eth.getTransactionCount(AddrChecksum)
+print("4. 논스 - 확인여부 :", nonce)
+
+value = web3.toWei(0.05, 'ether')
+print("5. 보내는금액 - 확인여부 :", value)
+
+tx = {'nonce': nonce,
+      'to': '0xf1337471403D2D7E7a83166c5E48dd619ecc95c3',
+      'value': value,
+      'gas': 21000,
+      'gasPrice': getGasPrice,
+      'chainId': 80001
+      }
+
+signed_tx = web3.eth.account.signTransaction(tx, privKey)
+print("6. 트랜잭션 - 확인여부 :", signed_tx)
+
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+print("7. 트랜잭션 전송 - 확인여부 :", web3.toHex(tx_hash))
+
+# unLock = web3.geth.personal.unlockAccount(AddrChecksum,'asd123!',10)
+# lock = web3.geth.personal.lockAccount(AddrChecksum)
+# time.sleep(10)
+# afterBal = web3.eth.getBalance(AddrChecksum)
+# value = web3.fromWei(afterBal,'ether')
+# print('잔액은', value, 'ETH')
